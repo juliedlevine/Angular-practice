f93a3fa68fe2a7b6eb8da624b5b01623cb098dfd
juliemdyer <juliemdyer@gmail.com>
2017-06-29 17:18:50 -0400

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryEntryService } from './backend';
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent } from './entries'
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        InMemoryWebApiModule.forRoot(InMemoryEntryService)
+    ],
+    providers: [
+        EntryService
+    ],
+    declarations: [
+        AppComponent,
+        EntryComponent,
+        EntryListComponent,
+        EntryCommentFormComponent,
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
